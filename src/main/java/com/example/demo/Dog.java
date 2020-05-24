8c3bdb4237eb5c3727683671fd5d0fc1b459696c
fitsumtsehay <tsehayfitsum@gmil.com>
2020-05-24 17:20:09 -0400

    SpringBoot_3.01

---
+++ src/main/java/com/example/demo/Dog.java
@@ -0,0 +1,49 @@
+package com.example.demo;
+
+import com.sun.istack.NotNull;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+
+@Entity
+
+public class Dog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+
+    private long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Min(1)
+    private Integer age;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
